20d7326da34f58719e60e1f7d9914fe8fed040b5
Ian Gregg <iangregg188@gmail.com>
2020-06-04 10:49:04 -0700

    Created functionality within our form through JS and CSS
    Co-authored-by: Ian Scott <chesnekov@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,26 @@
 $(document).ready(function(){
-  
-}
+
+  $("form#choose").submit(function(event) {
+    event.preventDefault();
+    
+    const animal = $("#animalchoice").val()
+
+    if (animal === "Lockness Monster") {
+      $(".animal1").show();
+      $(".animal2").hide();
+      $(".animal3").hide();
+    }
+    else if (animal === "Big Foot") {
+      $(".animal2").show();
+      $(".animal1").hide();
+      $(".animal3").hide();
+    }
+    else if (animal === "Chupacabra") {
+      $(".animal3").show();
+      $(".animal1").hide();
+      $(".animal2").hide();
+    }
+
+  });
+
+});
